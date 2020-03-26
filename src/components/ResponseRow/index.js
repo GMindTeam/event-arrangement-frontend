58c71101e6c867788b9d9e71bb67f9a5f0faa786
Athena195 <minhphuonga4k98@gmail.com>
2020-03-26 18:00:50 +0700

    finish createresponse, editresponse except validation of radio button

--- src/components/ResponseRow/index.js
+++ src/components/ResponseRow/index.js
@@ -1,25 +1,48 @@
-import React ,{useEffect}from "react";
+import React, { useState, useEffect } from "react";
 
 
 function ResponseRow(props) {
-useEffect(() => {
-  return () => {
-    
-  }
-}, [])
+  const[answer, setAnswer] = useState(props.row.answer);
+  useEffect(() => {
+    console.log(props.row)
+    return () => {
+
+    }
+  }, [])
 
 
   return (
     <tr>
-      <td>{props.title.content}</td>
+      <td>{props.row.content}</td>
       <td>
-        <input type="radio" name={props.title.content} />
+        <input type="radio"
+          name={props.row.content}
+          checked={(answer === "1")}
+          onChange={() => {
+            setAnswer("1")
+            return props.handleChange("1");
+          }}
+        />
       </td>
       <td>
-        <input type="radio" name={props.title.content} />
+        <input type="radio"
+          name={props.row.content}
+          checked={(answer === "2")}
+          onChange={() => {
+            setAnswer("2")
+            return props.handleChange("2");
+          }}
+        />
       </td>
       <td>
-        <input type="radio" name={props.title.content} />
+        <input type="radio"
+          name={props.row.content}
+          checked={(answer === "3")}
+          onChange={() => {
+            setAnswer("3")
+            return props.handleChange("3");
+          }}
+        />
       </td>
     </tr>
   );
